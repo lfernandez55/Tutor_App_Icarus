cf9b355794ed25535656dae34967807471e5cff0
lfernandez55 <lfernandez@weber.edu>
2020-06-06 13:18:45 -0600

    Able to send basic json data to client.

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -4,11 +4,11 @@
 
 
 from flask import Blueprint, redirect, render_template, flash
-from flask import request, url_for, current_app, jsonify
+from flask import request, url_for, current_app, jsonify, json
 from flask_user import current_user, login_required, roles_required
 
 from app import db
-from app.models.user_models import User, Role
+from app.models.user_models import User, Role, Tutor, Time
 from app.forms.book_forms import BookForm
 from app.forms.main_forms import UserProfileForm
 
@@ -61,15 +61,47 @@ def schedule():
 
 # example url: http://127.0.0.1:2000/schedule_json?tutor=aaaa
 @main_blueprint.route('/schedule_json', methods={'GET'})
-def schedule_core_json():
+def schedule_json():
     print('in check guess')
     # minute 807 in https://scotch.io/bar-talk/processing-incoming-request-data-in-flask
     print('args:', request.args.get('tutor_id')) 
     # print(request.json) #print out the json object to the console
     # print(request.json['guess']) #print out the guess to the console
 
+    dayArray = [1, 2, 3, 4, 5, 6, 7]
+    slotArray = []
+    for day in dayArray:
+        slots = Time.query.filter(Time.time_day == day)
+        for slot in slots:
+            slot.overlap = False
+            for otherSlot in slots:
+                if slot.id != otherSlot.id:
+                    if (slot.time_start <= otherSlot.time_end) and (slot.time_end >= otherSlot.time_start):
+                        slot.overlap = True
+            # slotObj = {'day':slot.time_day, 'time_start':slot.time_start, 'time_end': slot.time_end}
+            # slotObj = {'day':slot.time_day}
+            # print('debug', json.dumps(slot, indent=4, sort_keys=True, default=str))
+            # jsonifiedSlot = json.dumps(slot, indent=4, sort_keys=True, default=str)
+            # slotArray.append(jsonifiedSlot)
+            slotObj = {}
+            # slotObj['time_day'] = slot.time_day
+            # slotObj['time_start'] = json.dumps(slot.time_start, indent=4, sort_keys=True, default=str)
+            # slotObj['time_start'] = json.dumps(slot.time_end, indent=4, sort_keys=True, default=str)
+            # ts = json.dumps(slot.time_start, indent=4, sort_keys=True, default=str)
+            # te = json.dumps(slot.time_end, indent=4, sort_keys=True, default=str)
+            ts = str(slot.time_start)
+            te = str(slot.time_end)
+            slotObj = {'day':slot.time_day, 'time_start':ts, 'time_end': te, 'overlap': slot.overlap}
 
+            # this next line does not work: time_start is not serializable:
+            # slotObj = {'day':slot.time_day, 'time_start':slot.time_start, 'time_end': slot.time_end}
 
-    hint = {'whitePegs':1, 'blackPegs':2} #create the hint as a dict
-    print("the hint:", hint) #print out the hint to the console
-    return jsonify(hint) #return the dict as a json
+            print(slotObj)
+            slotArray.append(slotObj)
+    # print(slotArray)
+    # jsonifiedObj = json.dumps(slotArray, indent=4, sort_keys=True, default=str)
+    return jsonify(slotArray)
+
+    # hint = { "firstname":"Joe", "age":23} #create the hint as a dict
+    # print("the hint:", hint) #print out the hint to the console
+    # return jsonify(hint) #return the dict as a json
