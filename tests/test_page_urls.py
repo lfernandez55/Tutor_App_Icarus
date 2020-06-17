9d87467688d222f32d2ab5bc6764322056e6b47a
lfernandez55 <lfernandez@weber.edu>
2020-06-16 18:53:37 -0600

    Update test_page_urls.py

--- tests/test_page_urls.py
+++ tests/test_page_urls.py
@@ -34,8 +34,42 @@ def test_page_urls(client):
     response = client.post(url_for('user.login'), follow_redirects=True,
                            data=dict(email='admin@example.com', password='Password1'))
     assert response.status_code==200
-    response = client.get(url_for('main.admin_page'), follow_redirects=True)
+    response = client.get(url_for('admin.admin_page'), follow_redirects=True)
     assert response.status_code==200
+
+    # users
+    response = client.get(url_for('admin.admin_list_users'), follow_redirects=True)
+    assert response.status_code==200
+
+    response = client.get(url_for('admin.admin_create_tutor'), follow_redirects=True)
+    assert response.status_code==200
+
+    # editing the admin. . .
+    response = client.get(url_for('admin.admin_edit_user', user_id=1), follow_redirects=True)
+    assert response.status_code==200
+    
+    # creating a tutor. . . .
+    response = client.post(url_for('admin.admin_create_tutor'), follow_redirects=True,
+                        data=dict(first_name='joe', last_name='mo', email='joen@example.com', password='Password1', tutor_phone='801-540-7777'))
+    assert response.status_code==200
+
+    # editing the tutor. . ..
+    response = client.get(url_for('admin.admin_edit_tutor', user_id=3), follow_redirects=True)
+    assert response.status_code==200
+
+
+    # roles
+    response = client.get(url_for('admin.admin_list_roles'), follow_redirects=True)
+    assert response.status_code==200
+
+    # courses
+    response = client.get(url_for('admin.admin_list_courses'), follow_redirects=True)
+    assert response.status_code==200
+
+    # languages
+    response = client.get(url_for('admin.admin_list_languages'), follow_redirects=True)
+    assert response.status_code==200
+
 
     # Logout
     response = client.get(url_for('user.logout'), follow_redirects=True)
