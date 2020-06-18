461a040de2d535eb9cf0b4eb6adae4d8437543a4
lfernandez55 <lfernandez@weber.edu>
2020-06-17 19:17:03 -0600

    A little cleanup. . . .

--- app/views/admin_views.py
+++ app/views/admin_views.py
@@ -26,37 +26,6 @@ def admin_list_users():
     
     return render_template('admin/admin_list_users.html', users=users)  
 
-# @admin_blueprint.route('/admin/create_user', methods=['GET', 'POST'] )
-# @roles_required('admin')  # Limits access to users with the 'admin' role
-# def admin_create_user():
-#     form = UserCustomForm()
-    
-#     # adding the full set of select options to the select list (this is different than determining the default/selected options above)
-#     rolesCollection = Role.query.all()
-#     role_list = []
-#     for role in rolesCollection:
-#         role_list.append(role.name)
-#     role_choices = list(enumerate(role_list,start=1))
-#     form.roles.choices = role_choices
-
-#     if form.validate_on_submit():
-#         user = User()
-#         user.first_name  = form.first_name.data
-#         user.last_name = form.last_name.data
-#         user.email = form.email.data
-#         user.roles = []
-#         for role_id in form.roles.data:
-#             roleObj = Role.query.filter(Role.id == role_id).first()
-#             user.roles.append(roleObj)
-#         # todo: add in some password validations
-#         user.password=current_app.user_manager.password_manager.hash_password(form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-
-#         flash('User Created!!', 'success')
-#         return redirect(url_for('admin.admin_list_users'))
-#     return render_template('admin/admin_create_user.html', form=form)
-
 @admin_blueprint.route('/admin/create_tutor', methods=['GET', 'POST'] )
 @roles_required('admin')  # Limits access to users with the 'admin' role
 def admin_create_tutor():
@@ -177,9 +146,6 @@ def admin_create_tutor():
 @roles_required('admin')  # Limits access to users with the 'admin' role
 def admin_edit_tutor(user_id):
     user = User.query.filter(User.id == user_id).first()
-    
-    print("XXXXXXXXXXXXXXXXXXXXX:")
-    print(user.first_name)
 
     # determining the options that the user has selected/checked
     current_roles = []
@@ -196,15 +162,6 @@ def admin_edit_tutor(user_id):
     for course in user.courses:
         current_courses.append(course.id)
 
-    # if user.tutor.tutor_phone is None:
-    #     current_phone = ""
-    # else:
-    #     current_phone = user.tutor.tutor_phone
-
-    # if user.tutor.display_in_sched is None:
-    #     current_display = ""
-    # else:
-    #     current_display = user.tutor.display_in_sched
     if user.tutor is None:
         form = TutorCustomForm(id=user.id, first_name=user.first_name, last_name=user.last_name, email=user.email, 
         roles=current_roles, languages=current_languages, courses=current_courses)
@@ -212,14 +169,6 @@ def admin_edit_tutor(user_id):
         form = TutorCustomForm(id=user.id, first_name=user.first_name, last_name=user.last_name, email=user.email, 
         roles=current_roles, languages=current_languages, courses=current_courses, phone=user.tutor.tutor_phone, display_in_sched=user.tutor.display_in_sched, dates=user.tutor.dates)
             
-
-    # form = TutorCustomForm(id=user.id, first_name=user.first_name, last_name=user.last_name, email=user.email, 
-    # roles=current_roles, languages=current_languages, courses=current_courses, phone=user.tutor.tutor_phone, display_in_sched=user.tutor.display_in_sched, dates=user.tutor.dates)
-    # form = TutorCustomForm(id=user.id, first_name=user.first_name, last_name=user.last_name, email=user.email, 
-    # roles=current_roles, languages=current_languages, courses=current_courses, phone=current_phone, display_in_sched=current_display, dates=user.tutor.dates)
-
-
-
     # adding the full set of select options to the select list (this is different than determining the default/selected options above)
     # the above is a subset of the below
     # the above is the checked boxes, the below is all the check boxes
@@ -244,12 +193,6 @@ def admin_edit_tutor(user_id):
         course_list.append(course.name)
     course_choices = list(enumerate(course_list,start=1))
     form.courses.choices = course_choices
-    # the below print dictionary statement outputs what is built, namely:
-    #   choices attribute: 'choices': [(1, 'Web 1400'), (2, 'CS 1000'), (3, 'Web 1430'), (4, 'Web 3200')]
-    #   data attribute: 'data': ['2', '3', '4']
-    # print("SHOW:", form.courses.__dict__)
-
-
 
     # instead of doing languages like roles I added the list of languages in the form. see admin_forms > languages
 
@@ -258,9 +201,6 @@ def admin_edit_tutor(user_id):
         return render_template('admin/admin_create_edit_tutor.html', form=form, time_state='manage_time', state='Edit')
 
     if form.remove_time.data:
-        print("DDDDDDDD", form.remove_time_id.data)
-        print("xxxxxxx", type(form.dates), len(form.dates))
-
         # since we are popping off items from the end of the list rather than the beginning the index of the item to remove is:
         index_of_item_to_remove = len(form.dates) - int(form.remove_time_id.data)
 
@@ -294,9 +234,6 @@ def admin_edit_tutor(user_id):
 
         user.languages = []
         for lang in form.languages:
-            # print('dir(lang) dumps the attributes in the object: ', dir(lang) )
-            # print("          ")
-            # print('lang.__dict__ dumps the attributes and values: :', lang.__dict__)
             if lang.checked is True:
                 langObj = Language.query.filter(Language.id == lang.data).first()
                 user.languages.append(langObj)
@@ -334,59 +271,9 @@ def admin_edit_tutor(user_id):
         db.session.add(user)
         db.session.commit()
 
-
-
-
-
         flash('User Updated!!', 'success')
         return redirect(url_for('admin.admin_list_users'))
     return render_template('admin/admin_create_edit_tutor.html', form=form, state='Edit')
-
-
-
-# @admin_blueprint.route('/admin/edit_user/<user_id>', methods=['GET', 'POST'] )
-# @roles_required('admin')  # Limits access to users with the 'admin' role
-# def admin_edit_user(user_id):
-#     user = User.query.filter(User.id == user_id).first()
-
-#     # see "secret sauce" at https://stackoverflow.com/questions/12099741/how-do-you-set-a-default-value-for-a-wtforms-selectfield
-#     # to have a particular option selected in a selectlist, the option has to be loaded into the form when it is first instantiated
-#     # likewise to have multiple options selected in a multipleselectlist the options have to be loaded into the form when it is first instantiated
-#     # the syntax for setting the selects varies:
-#     # selectlist example:  form = MyUserForm(roles=1) #the first option is preselected
-#     # selectmultiplelist example: form = MyUserForm(roles=[1,3]) #the first and third option are preselected
-    
-#     # determining the default options to be selected (notice how they are loaded when the form is instantiated)
-#     current_roles = []
-#     for role in user.roles:
-#         current_roles.append(str(role.id))
-
-#     form = UserCustomForm(id=user.id, first_name=user.first_name, last_name=user.last_name, email=user.email, roles=current_roles)
-
-#     # adding the full set of select options to the select list (this is different than determining the default/selected options above)
-#     rolesCollection = Role.query.all()
-#     role_list = []
-#     for role in rolesCollection:
-#         role_list.append(role.name)
-#     role_choices = list(enumerate(role_list,start=1))
-#     form.roles.choices = role_choices
-
-
-#     if form.validate_on_submit():
-#         user.first_name  = form.first_name.data
-#         user.last_name = form.last_name.data
-#         user.email = form.email.data
-#         user.roles = []
-#         for role_id in form.roles.data:
-#             roleObj = Role.query.filter(Role.id == role_id).first()
-#             user.roles.append(roleObj)
-#         if form.password.data != "":
-#             user.password=current_app.user_manager.password_manager.hash_password(form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('User Updated!!', 'success')
-#         return redirect(url_for('admin.admin_list_users'))
-#     return render_template('admin/admin_edit_user.html', form=form)
 
 
 @admin_blueprint.route('/admin/delete_user/<user_id>')
@@ -447,8 +334,6 @@ def admin_delete_role(role_id):
 
 
 
-
-
 ####################################################################################
 #############################Course Views###########################################
 
@@ -468,21 +353,15 @@ def admin_list_courses():
 def admin_create_course():
     error_msg = ""
     course = Course()
-    # users = User.query.join(UsersRoles).join(Role).filter(Role.name == 'student').all()
 
     if request.method == 'POST':
         # email validation
         other_course = Course.query.filter(Course.name == request.form['name'] ).first()
         if (other_course is not None) and (other_course.id != course.id):
-            # course.user_ids = request.form.getlist('users') # keeps appropriate users selected
             error_msg = "This course name is already being used"
             flash('This course name is already being used!!', 'error')
         else:
             course.name  = request.form['name']
-            # course.users = []
-            # for user_id in request.form.getlist('users'):
-            #     userObj = User.query.filter(User.id == user_id).first()
-            #     course.users.append(userObj)
             db.session.add(course)
             db.session.commit()
             flash('Course Created!!', 'success')
@@ -496,15 +375,6 @@ def admin_create_course():
 def admin_edit_course(course_id):
     error_msg=""
     course = Course.query.filter(Course.id == course_id).first()
-    # users = User.query.join(UsersRoles).join(Role).filter(Role.name == 'student').all()
-
-    
-
-    # using user.roles creates complications. so we make a new attribute instead. this
-    # is used in the form to select what roles are associated with the user
-    # course.user_ids = []
-    # for user in course.users:
-    #     course.user_ids.append(str(user.id))
     
     if request.method == 'GET':
         request.form.name = course.name
@@ -517,10 +387,6 @@ def admin_edit_course(course_id):
             flash('This course name is already being used!!', 'error')
         else:
             course.name  = request.form['name']
-            # course.users = []
-            # for user_id in request.form.getlist('users'):
-            #     userObj = User.query.filter(User.id == user_id).first()
-            #     course.users.append(userObj)
             db.session.add(course)
             db.session.commit()
             flash('Course Created!!', 'success')
@@ -554,15 +420,11 @@ def admin_list_languages():
         print(language.name)
     return render_template('admin/admin_list_languages.html', languages=languages) 
 
-
-
-
 @admin_blueprint.route('/admin/create_language', methods=['GET', 'POST'])
 @roles_required('admin')  
 def admin_create_language():
     error_msg = ""
     language = Language()
-    # users = User.query.join(UsersRoles).join(Role).filter(Role.name == 'student').all()
 
     if request.method == 'POST':
         # email validation
@@ -573,10 +435,6 @@ def admin_create_language():
             flash('This language name is already being used!!', 'error')
         else:
             language.name  = request.form['name']
-            # language.users = []
-            # for user_id in request.form.getlist('users'):
-            #     userObj = User.query.filter(User.id == user_id).first()
-            #     language.users.append(userObj)
             db.session.add(language)
             db.session.commit()
             flash('Language Created!!', 'success')
@@ -590,15 +448,6 @@ def admin_create_language():
 def admin_edit_language(language_id):
     error_msg=""
     language = Language.query.filter(Language.id == language_id).first()
-    # users = User.query.join(UsersRoles).join(Role).filter(Role.name == 'student').all()
-
-    
-
-    # using user.roles creates complications. so we make a new attribute instead. this
-    # is used in the form to select what roles are associated with the user
-    # language.user_ids = []
-    # for user in language.users:
-    #     language.user_ids.append(str(user.id))
     
     if request.method == 'GET':
         request.form.name = language.name
@@ -611,10 +460,6 @@ def admin_edit_language(language_id):
             flash('This language name is already being used!!', 'error')
         else:
             language.name  = request.form['name']
-            # language.users = []
-            # for user_id in request.form.getlist('users'):
-            #     userObj = User.query.filter(User.id == user_id).first()
-            #     language.users.append(userObj)
             db.session.add(language)
             db.session.commit()
             flash('Language Created!!', 'success')
@@ -637,19 +482,4 @@ def admin_delete_language(language_id):
 #############################End Language Views#######################################
 ####################################################################################
 
-# the below views are for testing roles
-# you must create teacher and student roles to test them
-@admin_blueprint.route('/admin/teacher_or_admin')
-@roles_required(['admin', 'teacher'])  # requires admin OR teacher role
-def admin_teacher_or_admin():
-    return "You have the right roles to access this page - it requires admin OR teacher roles"
 
-@admin_blueprint.route('/admin/teacher_and_admin')
-@roles_required('admin','teacher')  # requires admin AND teacher roles
-def admin_teacher_and_admin():
-    return "You have the right roles to access this view"
-
-@admin_blueprint.route('/admin/student')
-@roles_required('student')  
-def admin_student():
-    return "You have the right roles to access this page - requires student role"
